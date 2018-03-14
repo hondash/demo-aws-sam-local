3f5ce1e2bc748158d5517cb9ede7b0597778e623
Shuji Honda <honpya@gmail.com>
2018-03-14 16:11:00 +0900

    Add console.log

--- src/serverless-express/app.js
+++ src/serverless-express/app.js
@@ -6,18 +6,22 @@ const awsServerlessExpressMiddleware = require('aws-serverless-express/middlewar
 app.use(awsServerlessExpressMiddleware.eventContext())
 
 app.get('/', (req, res) => {
+    console.log("GET: /");
     res.json(req.apiGateway.event)
 })
 
 app.get('/aaa', (req, res) => {
+    console.log("GET: /aaa");
     res.json(req.apiGateway.event)
 })
 
 app.get('/bbb', (req, res) => {
+    console.log("GET: /bbb");
     res.json(req.apiGateway.event)
 })
 
 app.get('/ccc', (req, res) => {
+    console.log("GET: /ccc");
     res.json(req.apiGateway.event)
 })
 

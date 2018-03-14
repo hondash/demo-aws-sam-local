464db0363033e7b03baf682b2ae4498777ff502e
Shuji Honda <honpya@gmail.com>
2018-03-14 15:04:57 +0900

    modified all the paths to index.js

--- src/serverless-express/app.js
+++ src/serverless-express/app.js
@@ -6,7 +6,19 @@ const awsServerlessExpressMiddleware = require('aws-serverless-express/middlewar
 app.use(awsServerlessExpressMiddleware.eventContext())
 
 app.get('/', (req, res) => {
-  res.json(req.apiGateway.event)
+    res.json(req.apiGateway.event)
+})
+
+app.get('/aaa', (req, res) => {
+    res.json(req.apiGateway.event)
+})
+
+app.get('/bbb', (req, res) => {
+    res.json(req.apiGateway.event)
+})
+
+app.get('/ccc', (req, res) => {
+    res.json(req.apiGateway.event)
 })
 
 module.exports = app
